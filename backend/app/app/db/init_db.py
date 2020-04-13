d2b76afcef67ecaed1641eda260c21fabfb7cdf2
nakano <tohokamiemitame4185@gmail.com>
2020-04-13 22:00:51 +0900

    KindとPlaceのSeedを生成するコードを追加

--- backend/app/app/db/init_db.py
+++ backend/app/app/db/init_db.py
@@ -1,6 +1,8 @@
 from app import crud
 from app.core import config
 from app.schemas.admin import AdminCreate
+from app.schemas.kind import KindCreate
+from app.schemas.place import PlaceCreate
 
 # make sure all SQL Alchemy models are imported before initializing DB
 # otherwise, SQL Alchemy might fail to initialize relationships properly
@@ -21,3 +23,15 @@ def init_db(db_session):
             password=config.FIRST_SUPERUSER_PASSWORD,
         )
         admin = crud.admin.create(db_session, obj_in=admin_in)
+
+        kinds_ins = [
+            KindCreate(name=name) for name in
+            ('かさ', '電子機器', '文房具', 'ノート・ファイル', '衣類・タオル', '鍵・アクセサリー', '教科書・本', 'その他')
+        ]
+        crud.kind.create_multi(db_session, obj_ins=kinds_ins)
+
+        place_ins = [
+            PlaceCreate(name=name) for name in
+            ('AV講義室', 'AV演習室', '端末講義室', '端末演習室', '1Fロビー', '2Fロビー')
+        ]
+        crud.place.create_multi(db_session, obj_ins=place_ins)
